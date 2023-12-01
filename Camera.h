9b40ac6a77da44b383efbb909500094348762b66
GeorgeBacalu <georgebacalu83@gmail.com>
2023-12-01 14:22:50 +0200

    Add camera member constants

--- Camera.h
+++ Camera.h
@@ -5,4 +5,12 @@ class Camera
 public:
 
 private:
+	const float defaultZNear = 0.1f;
+	const float defaultZFar = 500.f;
+	const float defaultYaw = -90.f;
+	const float defaultPitch = 0.f;
+	const float defaultFoV = 45.f;
+
+	const float cameraSpeedFactor = 2.5f;
+	const float mouseSensitivity = 0.1f;
 };
