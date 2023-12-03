37de5fab44633b9084cd8c88f08480773fbf5813
CondreaIustin <iustin.condrea@student.unitbv.ro>
2023-12-02 22:49:04 -0800

    Added method Set in Camera class

--- Camera.cpp
+++ Camera.cpp
@@ -5,3 +5,25 @@ Camera::Camera(const int width, const int height, const glm::vec3& position)
 	startPosition = position;
 	Set(width, height, position);
 }
+
+void Camera::Set(const int width, const int height, const glm::vec3& position)
+{
+    this->isPerspective = true;
+    this->yaw = defaultYaw;
+    this->pitch = defaultPitch;
+
+    this->FoVy = defaultFoV;
+    this->width = width;
+    this->height = height;
+    this->zNear = defaultZNear;
+    this->zFar = defaultZFar;
+
+    this->worldUp = glm::vec3(0, 1, 0);
+    this->position = position;
+
+    lastX = width / 2.0f;
+    lastY = height / 2.0f;
+    bFirstMouseMove = true;
+
+    UpdateCameraVectors();
+}
--- Camera.h
+++ Camera.h
@@ -5,7 +5,7 @@ class Camera
 {
 public:
 	Camera(const int width, const int height, const glm::vec3& position);
-
+	void Set(const int width, const int height, const glm::vec3& position);
 private:
 	const float defaultZNear = 0.1f;
 	const float defaultZFar = 500.f;
