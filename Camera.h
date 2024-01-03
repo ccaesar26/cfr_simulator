bbcc064a895461341f0e6a9397b222125540413f
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2024-01-03 16:57:37 +0200

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -1,49 +1,103 @@
-#pragma once
-#include <glm.hpp>
+#ifndef CAMERA_H
+#define CAMERA_H
 
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <stdio.h>
+#include <math.h> 
+
+#include <glad/glad.h>
+#include <glfw3.h>
+//#include <GL/glew.h>
+
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <ext.hpp>
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Shader.h"
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+#include <vector>
+
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+// Default camera values
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 200.0f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 60.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
 class Camera
 {
 public:
-	Camera(const int width, const int height, const glm::vec3& position);
+    // camera Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
 
-	void Set(const int width, const int height, const glm::vec3& position);
-	void Reset(const int width, const int height);
-	void Reshape(int windowWidth, int windowHeight);
+    // euler Angles
+    float Yaw;
+    float Pitch;
+
+    // camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+    glm::mat4 GetViewMatrix();
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+    void ProcessMouseScroll(float yoffset);
+    void setViewMatrix(glm::vec3 pos);
+
+    // constructors
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) :
+        Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) :
+        Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+
+    void printPosition();
 
 private:
-	void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch);
-	void UpdateCameraVectors();
-
-	const float defaultZNear = 0.1f;
-	const float defaultZFar = 500.f;
-	const float defaultYaw = -90.f;
-	const float defaultPitch = 0.f;
-	const float defaultFoV = 45.f;
-
-	const float cameraSpeedFactor = 2.5f;
-	const float mouseSensitivity = 0.1f;
-
-	// Perspective properties
-	float zNear;
-	float zFar;
-	float FoVy;
-	int width;
-	int height;
-	bool isPerspective;
-
-
-	glm::vec3 startPosition;
-
-	glm::vec3 position;
-	glm::vec3 forward;
-	glm::vec3 right;
-	glm::vec3 up;
-	glm::vec3 worldUp;
-
-	// Euler Angles
-	float yaw;
-	float pitch;
-
-	bool bFirstMouseMove = true;
-	float lastX = 0.f, lastY = 0.f;
+    void updateCameraVectors();
+    glm::vec3 prevPos;
 };
+#endif
