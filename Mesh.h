d42883337bd62111ed8e21ec4578f9d39ebbace3
GeorgeBacalu <georgebacalu83@gmail.com>
2024-01-03 12:06:04 +0200

    Add Mesh class

---
+++ Mesh.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <glad/glad.h> // holds all OpenGL type declarations
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+#include "Shader.h"
+
+#include <string>
+#include <vector>
+
+#define MAX_BONE_INFLUENCE 4
+
+class Mesh
+{
+
+};
