cf3e2285af059d7b9cecb7b8ed350e9a47e20e75
GeorgeBacalu <georgebacalu83@gmail.com>
2024-01-03 12:29:16 +0200

    Provide Mesh member variables and methods

--- Mesh.h
+++ Mesh.h
@@ -37,5 +37,23 @@ struct Texture {
 
 class Mesh
 {
+public:
+	// mesh Data
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+	unsigned int VAO;
 
+	// constructor
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+
+	// render the mesh
+	void Draw(Shader& shader);
+
+private:
+	// render data 
+	unsigned int VBO, EBO;
+
+	// initializes all the buffer objects/arrays
+	void setupMesh();
 };
