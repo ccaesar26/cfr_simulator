e44871b0fe5531cc53379b5a2d5062ee285911ea
GeorgeBacalu <georgebacalu83@gmail.com>
2024-01-03 12:31:35 +0200

    Add constructor definition

---
+++ Mesh.cpp
@@ -0,0 +1,19 @@
+#include "Mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	// now that we have all the required data, set the vertex buffers and its attribute pointers.
+	setupMesh();
+}
+
+void Mesh::Draw(Shader& shader)
+{
+}
+
+void Mesh::setupMesh()
+{
+}
