58c3f1683e401e554400499a6c383e70cf20b0f0
CondreaIustin <iustin.condrea@student.unitbv.ro>
2024-01-03 13:30:56 -0800

    Implemented method loadModel

--- Model.cpp
+++ Model.cpp
@@ -7,5 +7,19 @@ Model::Model(string const& path, bool gamma) : gammaCorrection(gamma)
 
 void Model::loadModel(string const& path)
 {
+    // read file via ASSIMP
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    // check for errors
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
+    {
+        cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
+        return;
+    }
+    // retrieve the directory path of the filepath
+    directory = path.substr(0, path.find_last_of('/'));
+
+    // process ASSIMP's root node recursively
+    processNode(scene->mRootNode, scene);
 }
 
