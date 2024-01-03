dd9cb8a15ba6c859043e3c442dd848247f773155
CondreaIustin <iustin.condrea@student.unitbv.ro>
2024-01-03 13:18:05 -0800

    Added Model class

---
+++ Model.h
@@ -0,0 +1,22 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <stb_image.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "Mesh.h"
+#include "Shader.h"
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Model
+{
+
+};
+
+#endif
