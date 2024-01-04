8a5aab7c4929ed36c8b288df16719fab4b579240
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2024-01-04 09:53:53 +0200

    Shader::Init method

--- Shader.h
+++ Shader.h
@@ -1,9 +1,43 @@
 #pragma once
-#include <string>
+
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<cerrno>
+
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <stdio.h>
+#include <math.h> 
+
+//#include <GL/glew.h>
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glad/glad.h>
+#include <glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+#ifndef SHADER_H
+#define SHADER_H
 
 class Shader
 {
 private:
+	void Init(const char* vertexPath, const char* fragmentPath);
 	void CheckCompileErrors(unsigned shaderStencilTesting, std::string type);
+
+public:
+	unsigned int ID;
 };
 
+#endif
