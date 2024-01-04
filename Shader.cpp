27b1aba9f6b72ed8d501362bcb4b8dd1faecf5e8
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2024-01-04 09:32:48 +0200

    Shader::CheckCompileErrors method

--- Shader.cpp
+++ Shader.cpp
@@ -1,1 +1,27 @@
 #include "Shader.h"
+
+#include <iostream>
+#include <glad/glad.h>
+
+void Shader::CheckCompileErrors(unsigned shaderStencilTesting, std::string type)
+{
+	GLint success;
+	GLchar infoLog[1024];
+	if (type != "PROGRAM") 
+	{
+		glGetShaderiv(shaderStencilTesting, GL_COMPILE_STATUS, &success);
+		if (!success) 
+		{
+			glGetShaderInfoLog(shaderStencilTesting, 1024, NULL, infoLog);
+			std::cerr << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else {
+		glGetProgramiv(shaderStencilTesting, GL_LINK_STATUS, &success);
+		if (!success) 
+		{
+			glGetProgramInfoLog(shaderStencilTesting, 1024, NULL, infoLog);
+			std::cerr << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+}
--- Shader.h
+++ Shader.h
@@ -1,5 +1,9 @@
 #pragma once
+#include <string>
+
 class Shader
 {
+private:
+	void CheckCompileErrors(unsigned shaderStencilTesting, std::string type);
 };
 
