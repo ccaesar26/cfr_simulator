6062ba6d0d319e058f43aac0182573696a9b4a7a
GeorgeBacalu <georgebacalu83@gmail.com>
2024-01-05 11:01:11 +0200

    attach depth texture as FBO's depth buffer

--- Main.cpp
+++ Main.cpp
@@ -212,6 +212,13 @@ int main()
 	float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
 	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
+	// attach depth texture as FBO's depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 	return 0;
 }
 
