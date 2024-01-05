8189c496608050ce77412ea6fe59938105b85c2d
GeorgeBacalu <georgebacalu83@gmail.com>
2024-01-05 11:13:52 +0200

    change depth function

--- cfr_simulator.cpp
+++ cfr_simulator.cpp
@@ -355,6 +355,12 @@ int main()
 
 		// draw skybox as last
 		glDepthFunc(GL_LEQUAL);
+
+		// change depth function so depth test passes when values are equal to depth buffer's content
+		skyboxShader.use();
+		view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix
+		skyboxShader.setMat4("view", view);
+		skyboxShader.setMat4("projection", projection);
 	}
 
 	return 0;
